9563dc367acccceed4652ebdd5d6036ac22d32c7
harrison <hkussie@gmail.com>
2023-08-08 11:58:19 -0700

    add hooks folder and move hooks

--- src/components/App.js
+++ src/components/App.js
@@ -1,35 +1,5 @@
 import '../App.css';
-import { useQuery, gql } from '@apollo/client';
-
-// Create query: 
-const GET_DATA = gql`
-  query displayData {
-    locations {
-      id
-      name
-      description
-      photo
-    }
-  }
-`;
-
-// Create function to return and display data
-function DisplayData() {
-  const { loading, error, data } = useQuery(GET_DATA);
-
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error :(</p>;
-  return data.locations.map(({ id, name, description, photo }) => (
-    <div key={id}>
-      <h3>{name}</h3>
-      <img width="400" height="250" alt="location-reference" src={`${photo}`} />
-      <br />
-      <b>About this location:</b>
-      <p>{description}</p>
-      <br />
-    </div>
-  ));
-}; 
+import DisplayData from '../hooks/fetchData';
 
 function App() {
   return (
